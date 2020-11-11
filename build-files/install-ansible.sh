f56b7892aa321d7ac668c84400fe1fed92b6aaa9
waqasnazir03 <wnazir.vf.ic.pk@gmail.com>
2020-11-11 12:14:25 -0500

    ansible version

--- build-files/install-ansible.sh
+++ build-files/install-ansible.sh
@@ -4,7 +4,7 @@
 if [[ "$RELEASE" == "rocky" || "$RELEASE" == "stein" ]]; then
   apt-get install -y ansible
 elif [[ "$RELEASE" == "train" ]]; then
-  python -m pip install --no-cache-dir "ansible>=2.8<3"
+  python -m pip install --no-cache-dir "ansible==2.9.7"
 else
   echo "ERROR: Unsupported environment var value for \$RELEASE: '$RELEASE'"
   echo "       Expected: rocky, stein, train"
